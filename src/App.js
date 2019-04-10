76b54ac283682b96dbf6af61168bd2df1538328d
ramshaessa <ressa@luc.edu>
2019-04-09 20:34:18 -0500

    frontend

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
+
+import Calendar from "./components/Calendar";
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header>
+	 <div id= "logo">
+		<span ClassName="icon"></span>
+		<span>
+		Shift<b>Ease</b>
+		</span>
+	</div>
+       </header>
+       <main>
+	<Calendar />
+       </main>
       </div>
-    );
-  }
-}
-
+     );
+    }
+   }
 export default App;
---
+++ src/components/list.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import dateFns from "date-fns";
+import { List, ListItem } from 'react-native-elements'
+
+const list = [
+    {
+      name: 'Ramsha',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      subtitle: 'Manager'
+    },
+    {
+      name: 'Ramsha',
+      avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+      subtitle: 'Barista'
+    },
+
+
+  renderEmployees({ item }) {
+    <ListItem
+      roundAvatar
+      title={item.name}
+      subtitle={item.subtitle}
+      avatar={{uri:item.avatar_url}}
+  ]
